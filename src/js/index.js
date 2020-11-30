b57c64ce18bad25c15926ccbf374b939f1675a4e
Anvar <anvarsam@mail.ru>
2020-12-01 00:18:06 +0500

    add few changs

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+// Main js file
+// "use strict";
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// Swiper.use([Navigation, Pagination]);
+// import 'swiper/swiper-bundle.css';
+
+// let swiperSert, swiperReview;
+
+// swiperSert = new Swiper('.sert__slider', {
+//     direction: 'vertical',
+//     loop: true,
+// }); 
+
+// swiperReview = new Swiper('.review__slider', {
+//     slidesPerView: 1,
+//     slidesPerColumn: 1,
+//     spaceBetween: 0
+// }); 
