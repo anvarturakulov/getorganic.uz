19a7fa032068cdbd1a6dc0cffc86ff526ee1d4cb
Anvar <anvarsam@mail.ru>
2020-12-01 12:08:09 +0500

    work with mobile adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 // Main js file
-// "use strict";
+"use strict";
 
 // import Swiper, { Navigation, Pagination } from 'swiper';
 // Swiper.use([Navigation, Pagination]);
@@ -17,3 +17,20 @@
 //     slidesPerColumn: 1,
 //     spaceBetween: 0
 // }); 
+
+
+let menuBtn = document.querySelector('.menu__btn');
+let menu = document.querySelector('.top__menu');
+let topMenuBox = document.querySelector('.top__menu-box');
+
+menuBtn.addEventListener('click',()=>{
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none';
+        topMenuBox.style.height = '60px';
+        topMenuBox.style.opacity = '0.8';
+    } else {
+        menu.style.display = 'block';
+        topMenuBox.style.height = '240px';
+        topMenuBox.style.opacity = '1';
+    }
+});
